b77e8aa7256583a4bd52e07b0d15648c5dc6eb4e
Gulfam Ali <40787109+gulfamali@users.noreply.github.com>
2023-01-30 12:45:27 +0530

    delego contract

---
+++ Delego.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+interface IERC20 {
+    function transfer(address recipient, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+contract Delego {
+    function tokenTransfer(address token, address recipient, uint256 value) external {
+        IERC20 erc20 = IERC20(token);
+        require(erc20.transferFrom(msg.sender, address(this), value));
+        require(erc20.transfer(recipient, value));
+    }
+}
